ab4e76729124e17af8be1180a334b43d416ab389
Andrea Toston <tostonar@gmail.com>
2019-08-10 23:44:09 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -17,3 +17,9 @@ def help
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
 end
+
+def list(songs)
+  songs.each_with_index { |song, index|
+    puts "#{index + 1}. #{song}"
+  }
+end
